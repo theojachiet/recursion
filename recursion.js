a926a12036ad237d1fa02ef8e66dbd97d9399f2b
Teoslip <tjachiet@gmail.com>
2025-07-25 09:17:29 +0200

    solving fibonnaci the iterative way

--- recursion.js
+++ recursion.js
@@ -1,11 +1,13 @@
 function fibs(n) {
-    if (n < 2) {
-        if (n === 0) {
-            return [];
-        } else {
-            return [0];
-        }
+    if (n === 0) return [];
+    if (n === 1) return [0];
+
+    let arr = [0, 1];
+    for (let i = 2; i < n; i++) {
+        arr.push(arr[i - 1] + arr[i - 2]);
     }
+
+    return arr;
 }
 
-console.log(fibs(n));
+console.log(fibs(8));
